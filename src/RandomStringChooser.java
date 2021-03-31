ccfbeee57e961a81fbf16b13f923610e6ea6af40
kchung1068 <kchung1068@stu.d214.org>
2021-03-31 15:16:09 -0500

    Gt

---
+++ src/RandomStringChooser.java
@@ -0,0 +1,28 @@
+public class RandomStringChooser {
+   private String strings[] = {""};
+
+
+    public RandomStringChooser() {
+     this.strings = new String[0];
+     this.strings[0] = "Word";
+    }
+
+    public RandomStringChooser(String[] strings) {
+
+      this.strings = strings;
+    }
+    public String getNext() {
+     String[] list = strings;
+     String toReturn = "";
+     int randomNumber = (int)(Math.random() * list.length);
+
+         while ( !(strings[randomNumber].equals("NONE"))) {
+          toReturn = strings[randomNumber];
+          strings[randomNumber] = "NONE";
+          return toReturn;
+
+     }
+      return "NONE";
+    }
+
+}
